da48b286946a9790a76ae8774e9ec1d1a87b86a3
Michael Harrington <mike85h@gmail.com>
2017-09-12 14:32:00 -0600

    initial

---
+++ index.js
@@ -0,0 +1,15 @@
+let arr1 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+
+function bubbleSort(arr){
+   var len = arr.length;
+   for (var i = len-1; i>=0; i--){
+     for(var j = 1; j<=i; j++){
+       if(arr[j-1]>arr[j]){
+           var temp = arr[j-1];
+           arr[j-1] = arr[j];
+           arr[j] = temp;
+        }
+     }
+   }
+   return arr;
+}
